1aac84ac4369b89ef09e60ee7255a1e2b16e9659
prernadave <prernadave3914@gmail.com>
2023-01-11 01:44:02 +0530

    Fibonacci done day-1

---
+++ Fibonacci.js
@@ -0,0 +1,19 @@
+/*Given a number "n" find the first "n" elements of 
+the fibonacci sequence*/
+
+/*Fibonnaci is a sequence in which each number is a sequence
+of the two preceding ones*/
+
+// the first two numbers in the sequence are 0 and 1
+
+function fibonacci(number){
+    const fib= [0,1];
+    for (let i =2; i<number ; i++){
+        fib[i]= fib[i-1] + fib[i-2]
+    }
+    return fib
+}
+
+console.log(fibonacci(5)); //[ 0, 1, 1, 2, 3 ]
+
+// time complexity =>o(n) - linear
