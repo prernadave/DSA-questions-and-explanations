260e57f6ab0c11401769fed7a33a2022f5d9689a
prernadave <prernadave3914@gmail.com>
2023-06-06 23:27:29 +0530

    added new preoblem

--- practice 201 assignments/constructor problem.js
+++ practice 201 assignments/constructor problem.js
@@ -6,20 +6,66 @@
 // gets name and age (everything that was sticked to this of the function human)
 
 function human() {
-    this.name = 'Will Smith';
-    this.age = 54;
+  this.name = 'Will Smith';
+  this.age = 54;
+}
+
+function Actor(movieName) {
+  this.movie = movieName;
+  human.call(this)
+
+  // call human function here in such a way that 
+  // the `this` object inside of the this function Actor
+  // gets name and age (everything that was sticked to this of the function human) 
+}
+
+
+let a1 = new Actor('Aladdin');
+console.log(a1.name); // should return Will Smith
+console.log(a1.age); // should return 54
+
+
+/**
+ * Problem 2
+* Create a class called Animal
+* the object created using Animal class is expected to have the following properties:
+* - name: the value will be passed while calling Animal with the new keyword
+* 
+* the object is expected to have following methods
+* - run(): console logs '<name of the animal> is running'
+* - stop(): console logs '<name of the animal> has stopped' 
+* 
+* extend Animal to create a Rabbit class
+* - The object created using Rabbit class has a name, it can run & it can stop
+* - it also has a method called hide which console logs '<name of the animal> has hidden'
+*/
+
+
+class Animal {
+  constructor(name) {
+    this.name = name
+
   }
-  
-  function Actor(movieName) {
-    this.movie = movieName;
-    human.call(this)
-  
-    // call human function here in such a way that 
-    // the `this` object inside of the this function Actor
-    // gets name and age (everything that was sticked to this of the function human) 
+  run() {
+    console.log(`${this.name} is running.`)
   }
-  
-  
-  let a1 = new Actor('Aladdin');
-  console.log(a1.name); // should return Will Smith
-  console.log(a1.age); // should return 54
+  stop() {
+    console.log(`${this.name} has stopped`)
+  }
+}
+
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name)
+  }
+  hide() {
+    console.log(`${this.name} has hidden`)
+  }
+}
+
+
+let r1 = new Rabbit('Ruby');
+r1.hide() // Ruby has hidden
+r1.run() // Ruby is running
+
